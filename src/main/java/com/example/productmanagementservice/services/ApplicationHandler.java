e143ce66e7ac4a523d5c7a0c9321b09df67632fb
venom7634 <venom7634@mail.ru>
2018-07-11 10:43:10 +0300

    Added check limit, amount and time in month on 0

--- src/main/java/com/example/productmanagementservice/services/ApplicationHandler.java
+++ src/main/java/com/example/productmanagementservice/services/ApplicationHandler.java
@@ -44,18 +44,24 @@ public class ApplicationHandler {
         if(verificationDatabase.verificationOnExistsApplication(token, id)){
             databaseHandler.addCreditCardToApplication(id,limit);
             responseEntity = new ResponseEntity<>(HttpStatus.OK);
-        } else {
+        } else if (limit < 0) {
+            responseEntity = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        else {
             responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         return responseEntity;
     }
+
     public ResponseEntity<String> addCreditCashToApplication(String token, long id, int amount, int timeInMonth) {
         ResponseEntity<String> responseEntity;
 
         if(verificationDatabase.verificationOnExistsApplication(token, id)){
             databaseHandler.addCreditCashToApplication(id,amount,timeInMonth);
             responseEntity = new ResponseEntity<>(HttpStatus.OK);
+        } else if (amount <= 0 || timeInMonth <= 0) {
+            responseEntity = new ResponseEntity<>(HttpStatus.FORBIDDEN);
         } else {
             responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
