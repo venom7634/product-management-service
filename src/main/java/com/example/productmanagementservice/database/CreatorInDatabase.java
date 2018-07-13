6ac0b95c3ca663666662eb4486521620dfb79e54
venom7634 <venom7634@mail.ru>
2018-07-13 12:16:09 +0300

    Delete commented code

--- src/main/java/com/example/productmanagementservice/database/CreatorInDatabase.java
+++ src/main/java/com/example/productmanagementservice/database/CreatorInDatabase.java
@@ -43,42 +43,4 @@ public class CreatorInDatabase {
     public void addTokenInDatabase(String token, String login) {
         jdbcTemplate.update("UPDATE users SET token = ? WHERE login = ?", token, login);
     }
-
-//    @PostConstruct
-//    public void createTables() {
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS users ");
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS applications ");
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS products ");
-//        jdbcTemplate.execute("CREATE TABLE users(" +
-//                "id  INTEGER PRIMARY KEY AUTOINCREMENT," +
-//                "login NCHAR(20) UNIQUE," +
-//                "password NCHAR(20) NOT NULL," +
-//                "token NCHAR(255)," +
-//                "security INTEGER NOT NULL," +
-//                "name NCHAR(20)  NOT NULL," +
-//                "description NCHAR(100))");
-//
-//        jdbcTemplate.execute("CREATE TABLE applications(" +
-//                "id  INTEGER PRIMARY KEY AUTOINCREMENT," +
-//                "client_id INTEGER NOT NULL," +
-//                "status INTEGER NOT NULL," +
-//                "product NCHAR(20)," +
-//                "limitOnCard INTEGER," +
-//                "amount INTEGER," +
-//                "timeInMonth INTEGER," +
-//                "description NCHAR(300)," +
-//                "FOREIGN KEY(client_id) REFERENCES users(id))");
-//
-//        jdbcTemplate.execute("CREATE TABLE products(" +
-//                "id  INTEGER PRIMARY KEY AUTOINCREMENT," +
-//                "name NCHAR(30) NOT NULL," +
-//                "description NCHAR(255) NOT NULL)");
-//
-//        jdbcTemplate.update("INSERT INTO users (login, password, token, security, name, description) " +
-//                "VALUES ('katya', '0502', '', '0', 'Katya', '3 years experience'), " +
-//                "('victor', '1234', '', '1', 'Victor', 'student')");
-//        jdbcTemplate.update("INSERT INTO products (name, description) VALUES ('debit-card', 'Regular client card')," +
-//                "('credit-card', 'This card have something amount money that you can spend')," +
-//                "('credit-cash', 'You are given the amount of money that you will return in a period of time')");
-//    }
 }
